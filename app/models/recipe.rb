d3904946b875590ba462d57630134d8620379fae
Nayel Velasco <18420051+nvelasco@users.noreply.github.com>
2023-08-23 14:09:53 +0200

    Add more spec to Recipe controller and some validations in Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,3 @@
 class Recipe < ApplicationRecord
+    validates :title, :description, presence: true
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -15,4 +15,99 @@ RSpec.describe 'Recipes', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      let!(:my_recipe) { FactoryBot.build(:recipe) }
+
+      before do
+        post '/recipes', params: { recipe: {
+          title: my_recipe.title,
+          description: my_recipe.description
+        }}
+      end
+
+      it 'returns the title' do
+        expect(json['title']).to eq(my_recipe.title)
+      end
+
+      it 'returns the title' do
+        expect(json['description']).to eq(my_recipe.description)
+      end
+
+      it 'returns a created status' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid parameters' do
+      before do
+        post '/recipes', params: { recipe: {
+          title: '',
+          description: ''
+        }}
+      end
+
+      it 'returns a unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'PUT /update' do
+    let!(:my_recipe) { FactoryBot.create(:recipe) }
+
+    context 'with valid parameters' do
+      let(:new_title) { 'new title' }
+      let(:new_description) {'new description' }
+
+      before do
+        put "/recipes/#{my_recipe.id}", params: { recipe: {
+          title: new_title,
+          description: new_description
+        }}
+      end
+
+      it 'returns the new title' do
+        expect(json['title']).to eq(new_title)
+      end
+
+      it 'returns the new description' do
+        expect(json['description']).to eq(new_description)
+      end
+
+      it 'returns a ok status' do
+        expect(response).to have_http_status(:ok)
+      end
+
+    end
+
+    context 'with invalid parameters' do
+
+      before do
+        put "/recipes/#{my_recipe.id}", params: { recipe: {
+          title: nil,
+          description: ''
+        }}
+      end
+
+      it 'returns a unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+  end
+
+  describe "DELETE /destroy" do
+    let!(:recipe) { FactoryBot.create(:recipe) }
+
+    before do
+      delete "/recipes/#{recipe.id}"
+    end
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+
 end
